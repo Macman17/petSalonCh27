0b7456854cebf1189721dc73324614c0885507b0
Macman17 <60798387+Macman17@users.noreply.github.com>
2022-03-15 20:05:20 -0700

    Updates created Table
    Created Table and it's functionality.

--- script/register.js
+++ script/register.js
@@ -1,5 +1,7 @@
 //create the constructor
-function Pet(name, age, gender,breed,owner,ownerPhone, service) {
+let x=0;
+function Pet(type,name, age, gender,breed,owner,ownerPhone, service) {
+    this.petType=type;
     this.petName=name;
     this.petAge=age;
     this.petGender=gender;
@@ -7,8 +9,10 @@ function Pet(name, age, gender,breed,owner,ownerPhone, service) {
     this.petOwner=owner;
     this.petOwnerPhone=ownerPhone;
     this.petService=service;
+    this.id=x++;
 }
 //get the info from the input
+let inputPetType= document.getElementById("txtPetType");
 let inputPetName= document.getElementById("txtPetName");
 let inputPetAge= document.getElementById("txtPetAge");
 let inputPetGender= document.getElementById("txtPetGender");
@@ -52,7 +56,7 @@ function register(){
             petSalon.pet.push(thePet);
             //display the obj in the 
             console.log(thePet);  
-            displayPetsName();
+            displayPetTable();
         } 
 }
 function displayPetsName() {
@@ -62,6 +66,7 @@ function displayPetsName() {
         //create the template
         tmp+=`
         <div class="pet">
+        
         <p>Name: ${petSalon.pet[i].petName}</p>
         <p>Age: ${petSalon.pet[i].petAge}</p>
         <p>Gender: ${petSalon.pet[i].petGender}</p>
@@ -69,6 +74,7 @@ function displayPetsName() {
         <p>Owner Name: ${petSalon.pet[i].petOwner}</p>
         <p>Owner Phone Number: ${petSalon.pet[i].petOwnerPhone}</p>
         <p>Service: ${petSalon.pet[i].petService}</p>
+        
         </div>
         
         `;
@@ -77,15 +83,75 @@ function displayPetsName() {
     }
     document.getElementById("pets").innerHTML=tmp;
 }
+function displayPetTable() {
+    let trow="";
+    for (let x = 0; x < petSalon.pet.length; x++) {
+        //create the template
+        trow+=`
+        <tr id="${petSalon.pet[x].id}">
+            <td>Name: ${petSalon.pet[x].petType}</d>
+            <td>Name: ${petSalon.pet[x].petName}</td>
+            <td>Age: ${petSalon.pet[x].petAge}</td>
+            <td>Gender: ${petSalon.pet[x].petGender}</td>
+            <td>Breed: ${petSalon.pet[x].petBreed}</td>
+            <td>Owner Name: ${petSalon.pet[x].petOwner}</td>
+            <td>Owner Phone Number: ${petSalon.pet[x].petOwnerPhone}</td>
+            <td>Service: ${petSalon.pet[x].petService}</td>
+            <td> <button onclick="deletePet(${petSalon.pet[x].id});">Delete</button> <td>
+        </tr>
+        
+        `;
+        
+        console.log(trow);
+    } 
+    document.getElementById("petTable").innerHTML=trow;
+}
+
+function deletePet(petId) {
+    console.log("Pet deleted:", petId);
+    //travel array
+    for (let x = 0; x < petSalon.pet.length; x++) {
+        let pet=petSalon.pet[x];
+        if (pet.id==petId) {
+            foundPet=x;
+            console.log("I found the pet", foundPet);
+        }
+        
+    }
+    //remove pet from array
+    petSalon.pet.splice(foundPet,1);
+    //remove pet from HTML
+    document.getElementById(petId).remove();
+
+    
+}
+function searchPet() {
+    let searchString=document.getElementById("txtSearch").value;
+    console.log(searchString);
+    for (let x = 0; x < petSalon.pet.length; x++) {
+        let pet = petSalon.pet[x];
+        if(pet.petName.toLowerCase()==searchString.toLowerCase()){
+            console.log("Found it!");
+            document.getElementById(pet.id).classList.add('bg-color');
+        }
+        else{
+            document.getElementById(pet.id).classList.remove('bg-color');
+        }
+         
+    }
+} 
 //display the tmp on the html
  
 function init() {
-    let haze = new Pet("Haze", 45, "Male", "Pitbull", "Amy", "888-888-8888", "Wash");
-let dexter = new Pet("Dexter", 34, "Male", "Lab","Glen","555-555-5555", "Wash and cut" );
-let tiger= new Pet("Tiger", 3, "Male","Great Dame","Glen","555-555-5555","Cut");
+let haze = new Pet("Dog","Haze", 45, "Male", "Pitbull", "Amy", "888-888-8888", "Wash");
+let dexter = new Pet("Dog","Dexter", 34, "Male", "Lab","Glen","555-555-5555", "Wash and cut" );
+let tiger= new Pet("Dog","Tiger", 3, "Male","Great Dame","Glen","555-555-5555","Cut");
 
     petSalon.pet.push(haze,dexter,tiger)
     displayPetsName();
+    displayPetTable();
 }
 window.onload=init;
+
+
 
